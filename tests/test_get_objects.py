42fb1e79455a6e7acf279d796b35532550e5156e
MirzayevMuzaffarjon <mirzayevmuzaffar525@gmail.com>
2025-04-03 15:26:28 +0500

    tests optimized

--- tests/test_get_objects.py
+++ tests/test_get_objects.py
@@ -9,4 +9,3 @@ def test_get_of_objects_by_ids(get_objects_api, product_id_list):
 def test_get_single_object(get_objects_api, product_id):
     response = get_objects_api.get_single_object(product_id)
     get_objects_api.verify_status_code_is(200, response.status_code)
-
