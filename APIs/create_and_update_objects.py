2e9b8f8bebdab6b0415c404984acb4b816178c41
MirzayevMuzaffarjon <mirzayevmuzaffar525@gmail.com>
2025-04-04 10:00:03 +0500

    added 2 new tests

--- APIs/create_and_update_objects.py
+++ APIs/create_and_update_objects.py
@@ -8,6 +8,14 @@ class CreateUpdateAPI(BaseAPI):
         url = f"{self.base_url}{self.endpoint}"
         return requests.post(url, json=payload)
 
+    def update_object(self, product_id, payload):
+        url = f"{self.base_url}{self.endpoint}/{product_id}"
+        return requests.put(url, json=payload)
+
+    def partially_update_object(self, product_id, payload):
+        url = f"{self.base_url}{self.endpoint}/{product_id}"
+        return requests.patch(url, json=payload)
+
     @staticmethod
     def get_product_id(json_data):
         product_id = json_data.get("id")
--- tests/test_create_and_update_objects.py
+++ tests/test_create_and_update_objects.py
@@ -8,8 +8,34 @@ payload_for_create_object = {
    }
 }
 
+payload_for_update_object = {
+   "name": "motorolla primary",
+   "data": {
+      "year": 2015,
+      "price": 150000,
+      "CPU model": "Intel Core i19",
+      "Hard disk size": "2 TB"
+   }
+}
+
+payload_for_partially_update_object = {
+   "name": "Nokia (Updated Name)"
+}
+
 
 def test_create_object(create_update_api):
     response = create_update_api.create_object(payload_for_create_object)
     create_update_api.verify_status_code_is(200, response.status_code)
     create_update_api.created_product_id = create_update_api.get_product_id(response.json())
+    print("\n---product created end created_product_id variable updated---")
+
+def test_update_object(create_update_api, product_id):
+    response = create_update_api.update_object(product_id, payload_for_update_object)
+    create_update_api.verify_status_code_is(200, response.status_code)
+    print("\n---product updated(put)---")
+
+def test_partially_update_object(create_update_api, product_id):
+    response = create_update_api.update_object(product_id, payload_for_partially_update_object)
+    create_update_api.verify_status_code_is(200, response.status_code)
+    print("\n---product updated(patch)---")
+
